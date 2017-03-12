5335d0621b06c9a9fef1616548c1aafe2bb37cfd
sgf <sgf.dma@gmail.com>
2017-03-12 15:38:01 +0300

    Several ways to run privileged commands.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Lib
     ( main_
@@ -9,12 +15,39 @@ import Data.Monoid
 import Data.Foldable
 import Data.Ini
 import Options.Applicative
-import qualified Data.Text      as T
-import qualified Data.Text.IO   as T
-import qualified Data.Map.Strict as S
-import qualified Data.Attoparsec.Text as A
+import qualified Data.Text              as T
+import qualified Data.Text.IO           as T
+import qualified Data.Attoparsec.Text   as T
+import qualified Data.Map.Strict        as M
 import Control.Monad
 import Control.Monad.Except
+
+import qualified Turtle as TH
+import qualified Shelly as SH
+import Development.Shake.Command
+import Sgf.Development.Shake.Command
+
+
+shPrivCmd :: (SH.CmdArg a, SH.ShellCmd b) => T.Text -> a -> b
+shPrivCmd u x = SH.cmd "sudo" ("-u" :: T.Text) u x
+
+shRootCmd :: (SH.CmdArg a, SH.ShellCmd b) => a -> b
+shRootCmd         = shPrivCmd "root"
+
+{-class ThPrivCmd m e | m -> e where
+    thPrivCmdC :: T.Text -> T.Text -> [T.Text] -> TH.Shell TH.Line -> m e
+
+instance ThPrivCmd IO TH.ExitCode where
+    thPrivCmdC = thPrivCmd
+
+instance ThPrivCmd TH.Shell TH.Line where
+    thPrivCmdC u cmd args = TH.inproc "sudo" (["-u", u, cmd] ++ args) empty-}
+
+thPrivCmd :: MonadIO m => T.Text -> T.Text -> [T.Text] -> TH.Shell TH.Line -> m TH.ExitCode
+thPrivCmd u cmd args inp = TH.proc "sudo" (["-u", u, cmd] ++ args) inp
+
+thRootCmd :: MonadIO m => T.Text -> [T.Text] -> TH.Shell TH.Line -> m TH.ExitCode
+thRootCmd         = thPrivCmd "root"
 
 
 -- | ACPI wakeup method name.
@@ -64,25 +97,25 @@ instance Read MethodState where
                 return (Off, t)
             | otherwise ->
                 []
-parseOn :: A.Parser T.Text
-parseOn         = asum . map A.string
+parseOn :: T.Parser T.Text
+parseOn         = asum . map T.string
                     $ ["On", "on", "enabled", "Enabled"]
-parseOff :: A.Parser T.Text
-parseOff        = asum . map A.string
+parseOff :: T.Parser T.Text
+parseOff        = asum . map T.string
                     $ ["Off", "off", "disabled", "Disabled"]
 -- | Parse method state from config file.
-parseMethodState :: A.Parser MethodState
+parseMethodState :: T.Parser MethodState
 parseMethodState = (pure On       <* parseOn) <|> (pure Off       <* parseOff)
 -- | Parse method state loaded from system. Returns 'LoadedOn' and
 -- 'LoadedOff'.
-loadMethodState :: A.Parser MethodState
+loadMethodState :: T.Parser MethodState
 loadMethodState  = (pure LoadedOn <* parseOn) <|> (pure LoadedOff <* parseOff)
 
 -- | Map with states for each wakeup method.
-type MethodMap      = S.Map MethodT MethodState
+type MethodMap      = M.Map MethodT MethodState
 -- | Map with list of states (loaded from different sources: system, config
 -- file, command-line arguments) for each wakeup method.
-type MethodMapL     = S.Map MethodT [MethodState]
+type MethodMapL     = M.Map MethodT [MethodState]
 
 -- | Parse ACPI wakeup file (@/proc/acpi/wakeup@ usually) and create a map
 -- with states of each method.
@@ -92,8 +125,8 @@ parseWakeup xm      = foldr go xm . T.lines
     go :: T.Text -> MethodMapL -> MethodMapL
     go l            =
         case filter (not . T.null) . T.split (`elem` [' ',  '\t']) $ l of
-          m : _ : "*enabled"  : _ -> S.insertWith (++) m [LoadedOn]
-          m : _ : "*disabled" : _ -> S.insertWith (++) m [LoadedOff]
+          m : _ : "*enabled"  : _ -> M.insertWith (++) m [LoadedOn]
+          m : _ : "*disabled" : _ -> M.insertWith (++) m [LoadedOff]
           _                       -> id
 
 -- | Generate options based on available methods on current system:
@@ -103,7 +136,7 @@ parseWakeup xm      = foldr go xm . T.lines
 --      /switch/ method from current value, but it does not allow to /specify/
 --      desired method state and leave for the program to apply it.
 generateOpts :: MethodMapL -> Parser MethodMapL
-generateOpts        = sequenceA . S.mapWithKey go
+generateOpts        = sequenceA . M.mapWithKey go
   where
     go :: MethodT -> [MethodState] -> Parser [MethodState]
     go k xs         = option (fmap (: xs) auto)
@@ -125,27 +158,37 @@ parseConfig ts zm0 ini = do
     go k zm
       | k `elem` ts = do
         x <- parseValue "Main" k parseMethodState ini
-        return (S.insertWith (++) k [x] zm)
+        return (M.insertWith (++) k [x] zm)
       | otherwise   = throwError $ "Unknown method "
                             ++ show k ++ " in ini file."
 
 -- | Set methods, which state differs from current system's value.
 workT :: MethodMap -> IO ()
 workT xs             = do
-    print (S.toList xs)
-    let ys = S.toList . S.filter isChanged $ xs
+    print (M.toList xs)
+    let ys = M.toList . M.filter isChanged $ xs
     print ys
+    --unit $ cmd
+    --unit $ rootCmd
+    --unit $ rootCmd ("echo" :: String) Shell ("huh?" :: String) (">" :: String) ("1.tmp" :: String)
+    --Stdout out <- rootCmd ("cat" :: String) (Stdin ("Ugh.." :: String)) Shell ("-" :: String) (">>" :: String) ("1.tmp" :: String) ("; cat 1.tmp" :: String)
+    --print (out :: String)
+    print "Rrrrrrrrrrrrrrr"
+    _ <- thRootCmd "echo" ["urk?", ">", "2.tmp"] empty
+    _ <- thRootCmd "cat" ["-", ">>", "2.tmp" ] "Brr.."
+    TH.proc "cat" ["2.tmp"] empty
+    return ()
 
 -- | Read system's ACPI methods and their states, then parse ini config file,
 -- then parse command-line arguments. The latter overrides the former.
 main_ :: FilePath -> FilePath -> IO ()
 main_ acFile cfFile = do
             c <- T.readFile acFile
-            let ac          = parseWakeup S.empty c
-                acMethods   = S.keys ac
+            let ac          = parseWakeup M.empty c
+                acMethods   = M.keys ac
             ini <- readIniFile cfFile
             cf  <- either error return (ini >>= parseConfig acMethods ac)
-            let opts = S.map (foldl1 addState) <$> generateOpts cf
+            let opts = M.map (foldl1 addState) <$> generateOpts cf
             join . execParser $
                 info (helper <*> (workT <$> opts))
                 (  fullDesc
---
+++ src/Sgf/Development/Shake/Command.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+{-|
+Module      : Sgf.Development.Shake.Command
+Description : Wrapper and type for running privileged commands.
+Maintainer  : sgf.dma@gmail.com
+
+-}
+module Sgf.Development.Shake.Command
+    ( Priv (..)
+    , commandAs
+    , commandAsRoot
+    , privCmd
+    , rootCmd
+    )
+  where
+
+import Data.Either
+import Development.Shake
+import Development.Shake.Command
+
+
+-- | Type for a command, which should run from different user.
+newtype Priv a      = Priv a
+  deriving (Show)
+
+-- | Similar to 'command', but for running as different user. Requires a
+-- /non-empty/ username. To run as @root@ without specifying username, use
+-- 'commandAsRoot'.
+commandAs :: CmdResult r => String              -- ^ User name to run command as.
+             -> Priv [Either CmdOption String]  -- ^ Command to run.
+             -> Action r
+commandAs []   _        = fail "Error, empty user name for privileged command."
+commandAs user (Priv x) = case partitionEithers x of
+    (opts, xs@(_ : _))  -> command opts "sudo" ("-u" : user : "--" : xs)
+    _                   -> error "Error, no executable given."
+
+-- | Just a shortcut for
+--
+--  > commandAs "root"
+commandAsRoot :: CmdResult r => Priv [Either CmdOption String] -> Action r
+commandAsRoot       = commandAs "root"
+
+-- | Similar to 'cmd', but for running as different user. Requires a
+-- /non-empty/ username. To run as @root@ without specifying username, use
+-- 'rootCmd'.
+--
+-- This function may be useful for running privileged command directly in `IO`
+-- monad instead of `Action` monad, because there is an
+--
+--  > instance CmdResult r => CmdArguments (IO r)
+--
+-- If i want to enforce an executable to run by @sudo@ to be given, i should
+-- define 'privCmd' like
+--
+--  > privCmd :: (Arg a, CmdArguments b) => String -> a -> b
+--
+-- but 'Arg' is not exported. So i let @sudo@ to fail with no command to run.
+privCmd :: CmdArguments b => String -> b
+--privCmd :: (Arg a, CmdArguments b) => String -> a -> b
+privCmd []          = error "Error, empty user name for privileged command."
+privCmd u           = cmd ["sudo", "-u", u]
+
+-- | Just a shortcut for
+--
+--  > privCmd root
+rootCmd :: CmdArguments b => b
+--rootCmd :: (Arg a, CmdArguments b) => a -> b
+rootCmd             = privCmd "root"
+
